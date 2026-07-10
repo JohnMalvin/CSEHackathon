bef993d95b0c089fc591dc48d59847ac87649990
Ddrewy <deandrew.y@gmail.com>
2026-07-10 21:15:46 +1000

    update: updating the mainPage

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,64 +1,23 @@
 "use client";
 
 import { useState } from "react";
+import React from "react";
 
-export default function RegisterPage() {
-	const [username, setUsername] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+export default function MainPage() {
+	const [search, setSearch] = useState('');
 
-	async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-		e.preventDefault();
-
-		const res = await fetch("/api/auth/register", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				username: "john",
-				email: "john@example.com",
-				password: "12345678",
-			}),
-		});
-
-		const data = await res.json();
-		console.log(data);
-
-		console.log("Status:", res.status);
-		console.log("Response:", data);
-
-		if (res.ok) {
-			alert("Registered successfully!");
-		} else {
-			alert(data.message ?? "Registration failed.");
-		}
+	function doSearch() {
+		//doSearch
 	}
-
+	
 	return (
-		<form onSubmit={handleSubmit}>
-			<input
-				type="text"
-				placeholder="Username"
-				value={username}
-				onChange={(e) => setUsername(e.target.value)}
-			/>
-
-			<input
-				type="email"
-				placeholder="Email"
-				value={email}
-				onChange={(e) => setEmail(e.target.value)}
-			/>
-
-			<input
-				type="password"
-				placeholder="Password"
-				value={password}
-				onChange={(e) => setPassword(e.target.value)}
-			/>
-
-			<button type="submit">Register</button>
-		</form>
+		<div className="flex">
+			<div className="absolute right-2 top-3">
+				<form className="pr-3">
+					<input value={search} onChange={e => setSearch(e.target.value)}/>
+					<button onClick={doSearch} className="">Search</button>
+				</form>
+			</div>
+		</div>
 	);
 }
